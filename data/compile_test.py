205b08b2ff0f97c7e25574305a545a1589c735ee
Chang Hahn <changhoon.hahn@princeton.edu>
2022-12-20 10:06:35 -0500

    compiled the training and testing data

--- data/compile_test.py
+++ data/compile_test.py
@@ -14,98 +14,27 @@ dat_dir = '/scratch/network/chhahn/sedflow/training_sed/'
 fsps = os.path.join(dat_dir, 'train.v0.1.101.thetas_sps.npy')
 funt = os.path.join(dat_dir, 'train.v0.1.101.thetas_unt.npy')
 
-f_Aspec = os.path.join(dat_dir, 'train.v0.1.101.norm_spec.nde_noise.npy') 
-f_hspec = os.path.join(dat_dir, 'train.v0.1.101.h_spec.reg_comp.nde_noise.npy') 
-f_Aivar = os.path.join(dat_dir, 'train.v0.1.101.norm_ivar.nde_noise.npy') 
-f_hivar = os.path.join(dat_dir, 'train.v0.1.101.h_ivar.nde_noise.npy') 
-
 f_zred = os.path.join(dat_dir, 'train.v0.1.101.redshifts.npy') 
 
-A_spec = np.load(f_Aspec)
-zero_norm = (A_spec == 0)
-
-theta_sps = np.load(fsps)[~zero_norm]
-theta_unt = np.load(funt)[~zero_norm]
-
-A_spec = A_spec[~zero_norm]
-h_spec = np.load(f_hspec)
-A_ivar = np.load(f_Aivar)[~zero_norm]
-h_ivar = np.load(f_hivar)[~zero_norm]
-
-zred = np.load(f_zred)[~zero_norm]
-
-################################################################
-# slight clean up  
-################################################################
-not_nan = (
-        ~(np.sum(np.isnan(A_spec)).astype(bool)) &
-        ~(np.sum(np.isnan(h_spec), axis=1).astype(bool)) & 
-        ~(np.sum(np.isnan(A_ivar)).astype(bool)) & 
-        ~(np.sum(np.isnan(h_ivar), axis=1).astype(bool))) 
-print('%i of %i have no NaNs' % (np.sum(not_nan), len(not_nan)))
-
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.theta_sps.npy',
-        theta_sps[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.theta_unt.npy', 
-        theta_unt[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.norm_spec.npy', 
-        A_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.h_spec.npy', 
-        h_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.norm_ivar.npy', 
-        A_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.h_ivar.npy', 
-        h_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.2.zred.npy', 
-        zred[not_nan]) 
-
-
-'''
-dat_dir = '/scratch/network/chhahn/sedflow/training_sed/'
-################################################################
-# compile thetas and obs
-################################################################
-fsps = os.path.join(dat_dir, 'train.v0.1.101.thetas_sps.npy')
-funt = os.path.join(dat_dir, 'train.v0.1.101.thetas_unt.npy')
-
-f_Aspec = os.path.join(dat_dir, 'train.v0.1.101.norm_spec.nde_noise.npy') 
-f_hspec = os.path.join(dat_dir, 'train.v0.1.101.h_spec.nde_noise.npy') 
-f_Aivar = os.path.join(dat_dir, 'train.v0.1.101.norm_ivar.nde_noise.npy') 
-f_hivar = os.path.join(dat_dir, 'train.v0.1.101.h_ivar.nde_noise.npy') 
+f_Aspec = os.path.join(dat_dir, 'train.v0.1.101.norm_spec.nde_noise.spender.npy') 
+f_hspec = os.path.join(dat_dir, 'train.v0.1.101.h_spec.nde_noise.spender.npy') 
+f_Ahivar = '/scratch/network/chhahn/sedflow/nde_noise/Ah_ivar.nde.101.npy'
 
 theta_sps = np.load(fsps)
 theta_unt = np.load(funt)
 
+zred = np.load(f_zred)
+
 A_spec = np.load(f_Aspec)
 h_spec = np.load(f_hspec)
-A_ivar = np.load(f_Aivar)
-h_ivar = np.load(f_hivar)
-
-f_zred = os.path.join(dat_dir, 'train.v0.1.101.redshifts.npy') 
-zred = np.load(f_zred)
+Ah_spec = np.concatenate([A_spec[:,None], h_spec], axis=1) 
+Ah_ivar = np.load(f_Ahivar)
 
 ################################################################
 # slight clean up  
 ################################################################
-not_nan = (
-        ~(np.sum(np.isnan(A_spec)).astype(bool)) &
-        ~(np.sum(np.isnan(h_spec), axis=1).astype(bool)) & 
-        ~(np.sum(np.isnan(A_ivar)).astype(bool)) & 
-        ~(np.sum(np.isnan(h_ivar), axis=1).astype(bool))) 
-print('%i of %i have no NaNs' % (np.sum(not_nan), len(not_nan)))
-
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.theta_sps.npy',
-        theta_sps[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.theta_unt.npy', 
-        theta_unt[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.norm_spec.npy', 
-        A_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.h_spec.npy', 
-        h_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.norm_ivar.npy', 
-        A_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.h_ivar.npy', 
-        h_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.1.zred.npy', 
-        zred[not_nan]) 
-'''
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.3.theta_sps.npy', theta_sps) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.3.theta_unt.npy', theta_unt) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.3.Ah_spec.npy', Ah_spec) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.3.Ah_ivar.npy', Ah_ivar) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.test.v0.3.zred.npy', zred) 
--- data/compile_training.py
+++ data/compile_training.py
@@ -10,131 +10,43 @@ import numpy as np
 
 
 dat_dir = '/scratch/network/chhahn/sedflow/training_sed/'
+
 ################################################################
 # compile thetas and observables 
 ################################################################
 theta_sps, theta_unt = [], [] 
-A_spec, h_spec, A_ivar, h_ivar, zred = [], [], [], [], [] 
+A_spec, h_spec, Ah_ivar, zred = [], [], [], []
 for ibatch in range(10): 
-    fsps = os.path.join(dat_dir, 'train.v0.1.%i.thetas_sps.npy' % ibatch)
-    funt = os.path.join(dat_dir, 'train.v0.1.%i.thetas_unt.npy' % ibatch)
+    fsps = os.path.join(dat_dir, f'train.v0.1.{ibatch}.thetas_sps.npy')
+    funt = os.path.join(dat_dir, f'train.v0.1.{ibatch}.thetas_unt.npy')
 
-    f_Aspec = os.path.join(dat_dir, 'train.v0.1.%i.norm_spec.nde_noise.npy' % ibatch) 
-    f_hspec = os.path.join(dat_dir, 'train.v0.1.%i.h_spec.reg_comp.nde_noise.npy' % ibatch) 
-    f_Aivar = os.path.join(dat_dir, 'train.v0.1.%i.norm_ivar.nde_noise.npy' % ibatch) 
-    f_hivar = os.path.join(dat_dir, 'train.v0.1.%i.h_ivar.nde_noise.npy' % ibatch) 
+    f_Aspec = os.path.join(dat_dir, f'train.v0.1.{ibatch}.norm_spec.nde_noise.spender.npy') 
+    f_hspec = os.path.join(dat_dir, f'train.v0.1.{ibatch}.h_spec.nde_noise.spender.npy') 
+
+    f_Ahivar = f'/scratch/network/chhahn/sedflow/nde_noise/Ah_ivar.nde.{ibatch}.npy'
     
-    f_zred = os.path.join(dat_dir, 'train.v0.1.%i.redshifts.npy' % ibatch) 
+    f_zred = os.path.join(dat_dir, f'train.v0.1.{ibatch}.redshifts.npy') 
     
-    _Aspec = np.load(f_Aspec)
-    zero_norm = (_Aspec == 0)
-
-    theta_sps.append(np.load(fsps)[~zero_norm])
-    theta_unt.append(np.load(funt)[~zero_norm])
-
-    A_spec.append(np.load(f_Aspec)[~zero_norm])
-    h_spec.append(np.load(f_hspec))
-    A_ivar.append(np.load(f_Aivar)[~zero_norm])
-    h_ivar.append(np.load(f_hivar)[~zero_norm])
-
-    zred.append(np.load(f_zred)[~zero_norm])
-
-theta_sps = np.concatenate(theta_sps, axis=0) 
-theta_unt = np.concatenate(theta_unt, axis=0) 
-
-A_spec = np.concatenate(A_spec, axis=0)
-h_spec = np.concatenate(h_spec, axis=0)
-A_ivar = np.concatenate(A_ivar, axis=0)
-h_ivar = np.concatenate(h_ivar, axis=0)
-zred = np.concatenate(zred, axis=0) 
-
-################################################################
-# slight clean up  
-################################################################
-not_nan = (
-        ~(np.sum(np.isnan(A_spec)).astype(bool)) &
-        ~(np.sum(np.isnan(h_spec), axis=1).astype(bool)) & 
-        ~(np.sum(np.isnan(A_ivar)).astype(bool)) & 
-        ~(np.sum(np.isnan(h_ivar), axis=1).astype(bool))) 
-print('%i of %i have no NaNs' % (np.sum(not_nan), len(not_nan)))
-
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.theta_sps.npy',
-        theta_sps[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.theta_unt.npy', 
-        theta_unt[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.norm_spec.npy', 
-        A_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.h_spec.npy', 
-        h_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.norm_ivar.npy', 
-        A_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.h_ivar.npy', 
-        h_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.2.zred.npy', 
-        zred[not_nan]) 
-
-'''
-dat_dir = '/scratch/network/chhahn/sedflow/training_sed/'
-################################################################
-# compile thetas
-################################################################
-theta_sps, theta_unt = [], [] 
-for ibatch in range(10): 
-    fsps = os.path.join(dat_dir, 'train.v0.1.%i.thetas_sps.npy' % ibatch)
-    funt = os.path.join(dat_dir, 'train.v0.1.%i.thetas_unt.npy' % ibatch)
-
     theta_sps.append(np.load(fsps))
     theta_unt.append(np.load(funt))
 
+    A_spec.append(np.load(f_Aspec))
+    h_spec.append(np.load(f_hspec))
+    Ah_ivar.append(np.load(f_Ahivar))
+
+    zred.append(np.load(f_zred))
+
 theta_sps = np.concatenate(theta_sps, axis=0) 
 theta_unt = np.concatenate(theta_unt, axis=0) 
 
-################################################################
-# compile observables 
-################################################################
-A_spec, h_spec, A_ivar, h_ivar, zred = [], [], [], [], [] 
-for ibatch in range(10): 
-    f_Aspec = os.path.join(dat_dir, 'train.v0.1.%i.norm_spec.nde_noise.npy' % ibatch) 
-    f_hspec = os.path.join(dat_dir, 'train.v0.1.%i.h_spec.nde_noise.npy' % ibatch) 
-    f_Aivar = os.path.join(dat_dir, 'train.v0.1.%i.norm_ivar.nde_noise.npy' % ibatch) 
-    f_hivar = os.path.join(dat_dir, 'train.v0.1.%i.h_ivar.nde_noise.npy' % ibatch) 
-
-    A_spec.append(np.load(f_Aspec))
-    h_spec.append(np.load(f_hspec))
-    A_ivar.append(np.load(f_Aivar))
-    h_ivar.append(np.load(f_hivar))
-
-    f_zred = os.path.join(dat_dir, 'train.v0.1.%i.redshifts.npy' % ibatch) 
-    zred.append(np.load(f_zred))
-
 A_spec = np.concatenate(A_spec, axis=0)
 h_spec = np.concatenate(h_spec, axis=0)
-A_ivar = np.concatenate(A_ivar, axis=0)
-h_ivar = np.concatenate(h_ivar, axis=0)
+Ah_spec = np.concatenate([A_spec[:,None], h_spec], axis=1) 
+Ah_ivar = np.concatenate(Ah_ivar, axis=0)
 zred = np.concatenate(zred, axis=0) 
 
-################################################################
-# slight clean up  
-################################################################
-not_nan = (
-        ~(np.sum(np.isnan(A_spec)).astype(bool)) &
-        ~(np.sum(np.isnan(h_spec), axis=1).astype(bool)) & 
-        ~(np.sum(np.isnan(A_ivar)).astype(bool)) & 
-        ~(np.sum(np.isnan(h_ivar), axis=1).astype(bool))) 
-print('%i of %i have no NaNs' % (np.sum(not_nan), len(not_nan)))
-
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.theta_sps.npy',
-        theta_sps[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.theta_unt.npy', 
-        theta_unt[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.norm_spec.npy', 
-        A_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.h_spec.npy', 
-        h_spec[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.norm_ivar.npy', 
-        A_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.h_ivar.npy', 
-        h_ivar[not_nan]) 
-np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.1.zred.npy', 
-        zred[not_nan]) 
-'''
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.3.theta_sps.npy', theta_sps) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.3.theta_unt.npy', theta_unt) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.3.Ah_spec.npy', Ah_spec) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.3.Ah_ivar.npy', Ah_ivar) 
+np.save('/scratch/network/chhahn/sedflow/sedflow_p.train.v0.3.zred.npy', zred) 
